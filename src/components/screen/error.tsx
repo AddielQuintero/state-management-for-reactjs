82b586a6cb03598cd449153562b6d3c0e601257f
atamayo <atamayoquintero@gmail.com>
2023-04-14 11:44:15 -0500

    feat: create error and update loading screen

--- src/components/screen/error.tsx
+++ src/components/screen/error.tsx
@@ -4,12 +4,12 @@ type Props = {}
 
 type State = {}
 
-export class Loading extends Component<Props, State> {
+export class Error extends Component<Props, State> {
   componentWillUnmount(): void {
     console.log('componentWillUnmount')
   }
-  
+
   render() {
-    return <p className="text-green-500 text-xl">Loading...</p>
+    return <p className="text-red-500 text-xs text-left">Incorrect code</p>
   }
 }
